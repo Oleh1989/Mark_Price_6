e13c95c6bfd809833cf1a6fa1b1c3440a1e5a2a2
Oleh Babkov <babkoff1989@gmail.com>
2019-07-30 12:23:21 +0300

    with operator

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PacktLibrary;
 using static System.Console;
 
 namespace PeopleApp
@@ -7,7 +8,20 @@ namespace PeopleApp
     {
         static void Main(string[] args)
         {
-            
+            var harry = new Person { Name = "Harry" };
+            var mary = new Person { Name = "Mary" };
+            var jill = new Person { Name = "Jill" };
+
+            var baby1 = mary.ProcreateWith(harry);
+            var baby2 = Person.Procreate(harry, jill);
+
+            // call operator
+            var baby3 = harry * mary;
+
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            WriteLine($"{mary.Name}'s first child is named \"{mary.Children[0].Name}\"");
         }
     }
 }
